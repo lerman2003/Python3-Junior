38b5b9b05fb4d02d13d0ad1aabeb21d19cb6002c
Creative studio Puzzle <43119921+StudioPuzzle@users.noreply.github.com>
2018-09-18 08:56:22 +0300

    Create polygon.py

---
+++ Lesson 1/polygon.py
@@ -0,0 +1,13 @@
+import turtle, random
+t=turtle.Pen()
+turtle.bgcolor("black")
+colors = ["red", "green", "blue", "pink", "purple", "yellow"]
+
+storona = int(turtle.numinput("Количество сторон", "Сколько сторон у Вашего многоугольника?",4, 3, 360))
+povtor = int(turtle.numinput("Количество повторений", "Сколько раз нарисовать многоугольники?",4, 1, 360))
+for x in range(povtor):
+    t.pencolor(random.choice(colors))
+    for i in range(storona):
+        t.forward(100)
+        t.left(360/storona)
+    t.left(360/povtor)
