1ab20258c72cc3144177475794fa4d6f3598f3d9
lerman2003 <43447959+lerman2003@users.noreply.github.com>
2018-09-22 15:20:34 +0300

    Add files via upload

---
+++ Lesson2/HomeWork2_(Leson2).py
@@ -0,0 +1,7 @@
+a=float(input("Какая длинна прямоугольника(а)?"))
+b=float(input("Какая ширина прямоугольника(b)?"))
+perimetr=float((a+b)*2)
+area=float(a*b)
+print("-------------")
+print("Периметр",perimetr)
+print("Площядь",area)
