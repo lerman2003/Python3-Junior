ab8fa789d61c81be79dce38829468cd6e608acde
lerman2003 <43447959+lerman2003@users.noreply.github.com>
2018-09-29 17:34:27 +0300

    Update and rename Lesson 1/HomeWork01.py to Урок 1/HomeWork01.py

--- Урок 1/HomeWork01.py
+++ Урок 1/HomeWork01.py
@@ -4,7 +4,7 @@ t.color("red")
 t.width(3)
 turtle.bgcolor("black")
 trio=int(turtle.numinput("Количество треугольников","Сколько треугольников нарисовать?",6,1,101))
-storona=int(turtle.numinput("Длина стороны","Какую длинну вы хотите задать?",50,1,1001))
+storona=int(turtle.numinput("Длина стороны","Какую длину вы хотите задать?",50,1,1001))
 for i in range(trio):
     for i in range(3):
         t.forward(storona)
