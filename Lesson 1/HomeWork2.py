a6bb79148c59ca12d6890b1ed9dc3e3359693092
lerman2003 <43447959+lerman2003@users.noreply.github.com>
2018-09-29 17:44:54 +0300

    Update HomeWork2.py

--- Lesson 1/HomeWork2.py
+++ Lesson 1/HomeWork2.py
@@ -1,4 +1,6 @@
-import turtle
+import turtle, random
+
+colors = ["red","orange","green","blue","pink","yellow"]
 t = turtle.Pen()
 t.color("red","green")
 t.width(3)
@@ -6,10 +8,9 @@ turtle.bgcolor("black")
 circle=int(turtle.numinput("Количество кругов","Сколько кругов нарисовать?",6,1,360))
 radius=int(turtle.numinput("Радиус круга","Какой радиус вы хотите задать?",50,1,100))
 for i in range(circle):
+    t.pencolor(random.choice(colors))
     t.circle(radius)
     t.left(360/circle)
-    
-
 
 
 
