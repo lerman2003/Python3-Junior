30b5f66051095ca650d83b94fe1f071d8d72688a
lerman2003 <43447959+lerman2003@users.noreply.github.com>
2018-09-29 17:45:58 +0300

    Update and rename Lesson 1/HomeWork3.py to Урок 1/HomeWork3.py

--- Урок 1/HomeWork3.py
+++ Урок 1/HomeWork3.py
@@ -1,4 +1,5 @@
-import turtle
+import turtle, random
+colors = ["red","orange","white", "green","blue",]
 t=turtle.Pen()
 turtle.bgcolor("black")
 t.color("red")
@@ -7,10 +8,10 @@ polygon=int(turtle.numinput("Количество многоугольников
 kolst=int(turtle.numinput("Количество сторон", "Сколько сторон задать многоугольнику?",4, 3, 250))
 for x in range(polygon):
     for i in range(kolst):
+        t.pencolor(random.choice(colors))
         t.forward(100)
         t.left(360/kolst)
     t.left(360/polygon)
-
 
 
 
